85587c4ca092d99d1833159f8a1491e6ef1f5d63
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-10-20 13:36:55 +0200

    Menu Options (sin programar y sin arte)

--- EscapeGames/Assets/Scripts/MainMenuManager.cs
+++ EscapeGames/Assets/Scripts/MainMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70d3ca149dfba52e13d40a1113009a08bd3e7b9347337a77abb0f7dea62eb944
-size 418
+oid sha256:51455a0bfb39c9eb9c4624496de377332768df522da20e8d2165f5d090f3cfc8
+size 735
--- EscapeGames/Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dc6bcb57d1d57f5a054142753f8e18c7774f242d27b914302d00db62c33ab40c
-size 4300
