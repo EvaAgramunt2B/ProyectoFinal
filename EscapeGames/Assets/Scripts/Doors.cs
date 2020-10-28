099719dc18ee0a72402d6ab4f1e967fb4d3961e4
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-10-28 18:15:25 +0100

    Por alguna razón ahora la taquilla no se cierra

--- EscapeGames/Assets/Scripts/Doors.cs
+++ EscapeGames/Assets/Scripts/Doors.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99968447545d42057ec285b81d12f9935b6ebac063250df56141aafb6937a3b1
-size 713
+oid sha256:87272b672d1f2513b61c9eaaec684d5b5642ff76386748ae13b9bc027acd71df
+size 678
