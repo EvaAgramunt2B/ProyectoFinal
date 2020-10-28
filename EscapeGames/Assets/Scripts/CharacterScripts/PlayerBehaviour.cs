4de2dc76069ab24d592f2252a4b8f778f90918d9
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-10-28 17:34:51 +0100

    Cosas un pelín más ordenadas (Volver a hacer Taquilla)

---
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:231de7b37b3742d250b4ea2de386c963812459e56fdb528be82243c5e64f6583
+size 1660
--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerMovement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3c9c93bbd65abff49796b5dd6c463254961f86d55d0f3606a13dccdf76c287db
-size 2037
