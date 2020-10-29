55bc053b67db9bda221716032d460ac091169b74
CristinaSantaines <cristinasantaines_bcnvj1mb1920@cev.com>
2020-10-29 19:07:04 +0100

    Quiero: llorar mucho
    Att: Vuestra Eva favorita

--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e793faaad10f855c2ee60fd494d33a0a73150bb34ad62d275930cc5fe62f3ccc
-size 1662
+oid sha256:213ece4946e3dea9ac2ea4586423599bf541e55a573feca4874617c271f3db57
+size 2437
