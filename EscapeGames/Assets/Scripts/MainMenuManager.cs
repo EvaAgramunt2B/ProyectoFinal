97a1fe84dd603ca9d055097eced2ff841e9eb626
CristinaSantaines <cristinasantaines_bcnvj1mb1920@cev.com>
2020-11-02 17:08:44 +0100

    MainMenu Musiquilla

--- EscapeGames/Assets/Scripts/MainMenuManager.cs
+++ EscapeGames/Assets/Scripts/MainMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51455a0bfb39c9eb9c4624496de377332768df522da20e8d2165f5d090f3cfc8
-size 735
+oid sha256:bad1aa9492c4630e72c8f7ad17830b1b95ee09653633063e1db3d5a68c65c644
+size 1364
