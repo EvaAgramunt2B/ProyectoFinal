9bf0c2c8a03e8a5bf5ee49d0975cc86a6c996ab6
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-11-23 16:57:22 +0100

    Key 01
    Key pero la key es un cubo que ni desaparece ni nada pq sino explota muy mucho

---
+++ EscapeGames/Assets/Scripts/BasementDrawerKey.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e408b8f40179882d8adfe5db3b6ff80c5a754071dd64a9418cf37ac3664d9df
+size 465
--- EscapeGames/Assets/Scripts/Doors.cs
+++ EscapeGames/Assets/Scripts/Doors.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:495334064f9d010e2dccde6fd884e68b4869d23e16614d46f989f36f25713612
-size 924
+oid sha256:e8446cff5e05bb3c72fa4db70ef1ebfb53c59d1b542fa6baa122f83f27f32820
+size 1731
