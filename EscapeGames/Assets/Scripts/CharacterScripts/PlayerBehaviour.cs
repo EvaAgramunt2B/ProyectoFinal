017c5ae2840787a265ba50c09450cd13a7f186f3
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2020-12-03 18:45:26 +0100

    quitado el codigo de flashlight en el jugador

--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aed9e3dc2df2cfddd0bb81b4d3ea12b383583b1df78318fa59b5f163386d50b2
-size 3972
+oid sha256:53f7c76726f99f30d0cccc32b0cbbfcb0ebdb5dbbce6c0a3595d4f4535a51dad
+size 3669
---
+++ EscapeGames/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7bda5a344a3452c764bb8be1df4cb89949db08002e140614289fc52e1c9db9eb
+size 297
