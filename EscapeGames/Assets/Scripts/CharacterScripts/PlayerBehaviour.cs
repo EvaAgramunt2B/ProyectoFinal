cf53428f7dfbf43a3c7145195356fbd92443a370
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2020-12-03 18:46:34 +0100

    se me olvidó quitar una cosilla

--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53f7c76726f99f30d0cccc32b0cbbfcb0ebdb5dbbce6c0a3595d4f4535a51dad
-size 3669
+oid sha256:2efa65af186b12442c1021356806615602e241cc9577ef342ad1b265ed953da9
+size 3592
