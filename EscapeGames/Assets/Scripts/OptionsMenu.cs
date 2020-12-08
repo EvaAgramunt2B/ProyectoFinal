35c817df6bfc618142fc19c3718ec7340fff17af
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-12-08 16:32:15 +0100

    Sonido va bien slider

--- EscapeGames/Assets/Scripts/OptionsMenu.cs
+++ EscapeGames/Assets/Scripts/OptionsMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18b6fb5b9ac28a292ce0f24eeef778e7ecbaff9a0958fd4b38eb199fdba23ded
-size 7128
+oid sha256:caaae2686cdd9ae656940bc8fc6080dc8db03879b793deb5b0463f37e6c4da00
+size 7330
