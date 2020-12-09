223c085bb71b5b9515a22711a65906a77dd20af6
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-12-09 14:52:11 +0100

    Programacion Num Pad + algunos props importados al proyecto

--- EscapeGames/Assets/Scripts/NumPad.cs
+++ EscapeGames/Assets/Scripts/NumPad.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e7ad797523291f48143d34a64fe00a52d9babdb5a3c6435a3b52cd33f60128b
-size 2248
+oid sha256:6145cc7c8bf5f6b37b3953e0e38cdd7bca7d210bab8f1696342e86a0c3174d29
+size 2666
