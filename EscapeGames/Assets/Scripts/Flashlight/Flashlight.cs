8799dd2d7b7d2c6481fd603fef78471236b2fb36
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-12-10 09:52:47 +0100

    linterna con godmode funciona

--- EscapeGames/Assets/Scripts/Flashlight/Flashlight.cs
+++ EscapeGames/Assets/Scripts/Flashlight/Flashlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d73e663e1cac5d6d74f6ea6b927f17f76c8277e140a8256e0c737dc7c27941fa
-size 1430
+oid sha256:74eaa355de29a001661e903da7191f4b14a7f66d07b8db321761ef4d004e1e76
+size 1605
