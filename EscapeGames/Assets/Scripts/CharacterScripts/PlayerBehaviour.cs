9440d622fccc7bf43e7587c1d056f92c318b5b6f
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-12-11 18:11:50 +0100

    he intentado arreglar lo de la gravedad pero no ha ido muy bien que digamos xd

--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:933bebd3690ff7bbb6b863cb3fd7114e1e8559a806bc15228edd8e9ecdce66f4
-size 3852
+oid sha256:68119ce4b8a126079b96f8facbb4ef06cdb054a4a466659c0db6f32647b1cf0f
+size 4256
