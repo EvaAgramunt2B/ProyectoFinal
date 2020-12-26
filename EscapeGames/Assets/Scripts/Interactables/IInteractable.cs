9db0f5a7a07ccfb018060c82f4943602cca76f3b
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2020-12-26 17:44:16 +0100

    lo mismo con el numpad

--- EscapeGames/Assets/Scripts/Interactables/IInteractable.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b61f2286c1639debebbc492c6d5c40e82c72ca83f04d42d15f4b0a26fff018f7
-size 250
---
+++ EscapeGames/Assets/Scripts/Interactables/NumPadInteractV2.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:649ffbc0814e720d155983a22982328d94ec2968899246a97f9c029c772810dc
+size 1090
