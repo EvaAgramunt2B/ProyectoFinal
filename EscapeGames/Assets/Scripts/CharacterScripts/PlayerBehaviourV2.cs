16878d9d1a319cf4f9d9d499de95a29edd132560
EvaAgramunt <evaagramunt_bcnvj1ma1920@cev.com>
2021-01-11 16:59:11 +0100

    no se que he hecho lmao

--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfc4496313283f4259134eb34e675915260f9b92e8610f69a756d945cc130255
-size 6403
+oid sha256:cea07b1f7eecd1693d4524a109c1ab85284658b397aba82c44ff8a5d3dca06f4
+size 6081
