8456249234f55bd533a5e89e9f7b638fa51f711f
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-01-19 00:29:44 +0100

    intentando OTRO SHADER MALDITA SEAAAAA

---
+++ EscapeGames/Assets/Scripts/Outliner/IBordereable.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a839da3bd1824ac5cf2a0de9c558f29acd3971387b8a34c2c3555fc91ff7f6c
+size 174
---
+++ EscapeGames/Assets/Scripts/Outliner/Outliner.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6440a70bd9779316d2a00be8054982c36f63cce6cda48cb5bb26c23ad51f3e70
+size 3414
