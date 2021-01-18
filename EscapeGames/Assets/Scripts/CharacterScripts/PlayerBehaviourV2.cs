fe17703760450e07a46e81450117b7177656b969
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-01-18 17:00:11 +0100

    proyector luz volumétrica

--- EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ EscapeGames/Assets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cea07b1f7eecd1693d4524a109c1ab85284658b397aba82c44ff8a5d3dca06f4
-size 6081
+oid sha256:a69420190eef2b2c1a5b952f3695ccd1e0d5b9eb3e26ce6239f3187b236b112a
+size 6085
