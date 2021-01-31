23acfb0d29f58df1b8463113096fc173c75e7820
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-01-31 12:53:09 +0100

    Logo blanco en main menu

--- EscapeGames/Assets/Scripts/Examine.cs
+++ EscapeGames/Assets/Scripts/Examine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cae44c3d3b66c9c46e857fff0fbbdc1886e3e4620ab4ad9e774c4663cea37b4
-size 1719
+oid sha256:7c948f2e7fdfe602ebbfc13797d6b011ef0f364d29db913272979da910c00027
+size 1736
