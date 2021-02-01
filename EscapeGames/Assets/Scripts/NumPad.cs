537c6ddac1953c11647a69644180af331b9fccb8
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-02-01 18:46:07 +0100

    he querido poner audios pero no van bien enfin me retiro

--- EscapeGames/Assets/Scripts/NumPad.cs
+++ EscapeGames/Assets/Scripts/NumPad.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7845bdd95d92a51d2d7c71eee2eed501e0134bb3c617d5c8ef845ce4929b753
-size 3961
+oid sha256:632d9127927f7b0b6d01c9c79261ea93e8865a8d6dfc0fb47824369ae5b856a9
+size 4155
