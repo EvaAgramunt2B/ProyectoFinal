9df343ad74e5ec90979e4cd38934f3e58ed273a1
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-02-02 00:44:29 +0100

    bombilla y luz de la bombilla

--- EscapeGames/Assets/Scripts/Flickerlight.cs
+++ EscapeGames/Assets/Scripts/Flickerlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ea91cb7433cc7c70c2dd73be1da26a2c9aa1f223356e460f201ae0281f9862a
-size 554
+oid sha256:89c26cbbe76ed5c17059f97acd13c5fa411691df80caf869154a060318e8bbb5
+size 813
