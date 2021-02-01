4b98755ef526ee6a79f38f338055765a3cf483b2
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-02-01 13:48:03 +0100

    jumpscare, hay que editar el audio pero la base está

---
+++ EscapeGames/Assets/AllSkyFree/AllSkyFree_Menu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d10e15da1ebf67693dbc60f1014035b6b669338cbd0069fe97f5e1ac346c6f5b
+size 397
---
+++ EscapeGames/Assets/Scripts/JumpScare.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b6b63ddecda1427517db9a6a617832999a80a4a4edfd527c1af2ece3e38323d
+size 512
