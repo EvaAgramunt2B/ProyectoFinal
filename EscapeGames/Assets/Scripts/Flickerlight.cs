433dec7934ca3a795832b9839c072b35eecf30d6
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-02-08 15:55:12 +0100

    Tonemapping, cambiado decal controles

--- EscapeGames/Assets/Scripts/Flickerlight.cs
+++ EscapeGames/Assets/Scripts/Flickerlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89c26cbbe76ed5c17059f97acd13c5fa411691df80caf869154a060318e8bbb5
-size 813
+oid sha256:ef598469b0a20bef025f1c991257349d69d200f89615367e2cf9fe45af83893c
+size 735
